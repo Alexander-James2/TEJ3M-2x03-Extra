cb523412071d78e740fef19bfaba502e1b59e8ff
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-10-26 10:46:15 -0400

    final code

--- code.py
+++ code.py
@@ -1,8 +1,13 @@
-from machine import Pin
-import utime
+# Created by Alexander James
+# Created on Oct. 2023
+# Uses the Pi Pico to blink an LED on a breabboard
 
-led = Pin(8, Pin.OUT)
-led.low()
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.GP8)
+led.direction = digitalio.Direction.OUTPUT
 
 while True:
     led.value = True
